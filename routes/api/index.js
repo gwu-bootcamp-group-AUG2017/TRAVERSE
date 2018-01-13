614032116e5878aebb8121857e4faf651d80b63c
John Hillegass <hillegassdev@gmail.com>
2018-01-13 10:58:59 -0500

    fix the load server bug

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,9 @@
 const router = require("express").Router();
 const articleRoutes = require("./articles");
-const locationRoutes = require("./places");
 const nytRoutes = require("./nyt");
 
 // NYT routes
 router.use("/articles", articleRoutes);
-
-router.use("/places", locationRoutes);
 
 router.use("/nyt", nytRoutes);
 
