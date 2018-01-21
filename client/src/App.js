c0286031e31d7816277dcc77b683021a3643f84f
David Thurman <dvdthurman@gmail.com>
2018-01-21 12:19:17 -0500

    merge APP js changes
    merge APP js changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,27 @@ import Home from "./pages/Home";
 import Saved from "./pages/Saved";
 import Location from "./pages/Location";
 import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import Nav from "./components/Nav"; 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Callback from './components/Callback';
 import { requireAuth } from './utils/authService';
-
 
 const App = () =>
   <Router>
     <div>
-      <Nav />
+      <Nav /> 
+      <Header />
+    
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Home} onEnter={requireAuth} />
+
         <Route exact path="/places" component={Location} onEnter={requireAuth} />
         <Route exact path="/saved" component={Saved}  onEnter={requireAuth} />
-        <Route path="/callback" component={Callback} />
+        <Route path="/callback" component={Callback} />                                                                 
         <Route component={NoMatch} />
       </Switch>
+         <Footer /> 
     </div>
   </Router>;
 
